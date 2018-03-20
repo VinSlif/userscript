2656075d5b839eff2e63d93b06440ac3a4f9022c
VinSlif <vincent.slifer@gmail.com>
2018-03-19 23:19:56 -0700

    Array and Object checks
    Added new methods to compare arrays and objects

--- Utility/Utilities.js
+++ Utility/Utilities.js
@@ -2,24 +2,18 @@
 var Util = {
     // Display custom console messages
     console: {
-        /* Custom console log message
-         * @param {String} message to be displayed
-         */
+        // Custom console log message
         log: function (msg) {
             console.log('%c' + GM_info.script.name + ':', 'color: blue', msg);
         },
-        /* Custom console error message
-         * @param {String} message to be displayed
-         */
+        // Custom console error message
         error: function (msg) {
             console.error('%c' + GM_info.script.name + ':', 'color: white; background-color: red', msg);
         },
     },
     // Parses strings to formats
     parse: {
-        /* Parses a string as an HTML document
-         * @param {String} string to convert
-         */
+        // Parses a string as an HTML document
         getHTML: function (str) {
             let tmp = document.implementation.createHTMLDocument();
             tmp.body.innerHTML = str;
@@ -28,10 +22,7 @@ var Util = {
     },
     // Math functions
     math: {
-        /* Gets a random number between two(2) supplied numbers
-         * @param {Int} minimum number
-         * @param {Int} maximum number
-         */
+        // Gets a random number between two(2) supplied numbers
         getRandomIntInclusive: function (min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
@@ -40,10 +31,7 @@ var Util = {
     },
     // Value initializers
     init: {
-        /* Sets all elements of an empty array to a value
-         * @param {Array} array to set
-         * @param value to set array to
-         */
+        // Sets all elements of an empty array to a value
         setAll: function (arr, val) {
             for (let i = 0, n = arr.length; i < n; i++) arr[i] = val;
         },
@@ -80,7 +68,7 @@ var Util = {
             // Grab style sheet
             styleSheet = styleEl.sheet;
 
-            for (var i = 0, rl = rules.length; i < rl; i++) {
+            for (let i = 0, rl = rules.length; i < rl; i++) {
                 var j = 1,
                     rule = rules[i],
                     selector = rules[i][0],
@@ -91,7 +79,7 @@ var Util = {
                     j = 0;
                 }
 
-                for (var pl = rule.length; j < pl; j++) {
+                for (let pl = rule.length; j < pl; j++) {
                     var prop = rule[j];
                     propStr += prop[0] + ':' + prop[1] + (prop[2] ? ' !important' : '') + ';\n';
                 }
@@ -103,9 +91,7 @@ var Util = {
     },
     // Custom events
     event: {
-        /* Detects when the page has loaded, including HTML5 history states and youtube
-         * @param {Function} function to execute when the page loads
-         */
+        // Detects when the page has loaded, including HTML5 history states and youtube
         onDocumentReady: function (fn) {
             document.addEventListener('DOMContentLoaded', fn); // one-time early processing
             window.addEventListener('load', fn); // one-time late postprocessing 
@@ -113,10 +99,7 @@ var Util = {
     },
     // Access GreaseMonkey or local storage, user defined
     store: {
-        /* Sets storage value with a key
-         * @param {String} storage key
-         * @param {Value} value to be stored
-         */
+        // Sets storage value with a key
         set: function (key, val) {
             val = JSON.stringify(val);
             if (typeof GM_setValue === 'undefined') {
@@ -124,9 +107,7 @@ var Util = {
             }
             return GM_setValue(key, val);
         },
-        /* Retrieves item in storage by a key
-         * @param {String} storage key
-         */
+        // Retrieves item in storage by a key
         get: function (key) {
             var res;
             if (typeof GM_getValue === 'undefined') {
@@ -140,9 +121,7 @@ var Util = {
                 return res;
             }
         },
-        /* Deletes item in storage by a key
-         * @param {String} storage key
-         */
+        // Deletes item in storage by a key
         del: function (key) {
             if (typeof GM_deleteValue === 'undefined') {
                 return localStorage.removeItem(key);
@@ -152,16 +131,83 @@ var Util = {
     },
     // Child elements
     child: {
-        /* Returns the child index of an element
-        * @param {Element} element index to find
-        */
+        // Returns the child index of an element
         getIndex: function (child) {
             var parent = child.parentNode;
-            for (var i = parent.children.length - 1; i >= 0; i--) {
-                if (child == parent.children[i]) {
-                    return i;
-                }
+            for (let i = parent.children.length - 1; i >= 0; i--) {
+                if (child == parent.children[i]) return i;
             }
         },
+    },
+    // Handles objects
+    object: {
+        // Checks if two (2) objects or arrays are equal to each other
+        isEquivalent: function (a, b) {
+            // Get the value type
+            var type = Object.prototype.toString.call(a);
+
+            // If the two objects are not the same type, return false
+            if (type !== Object.prototype.toString.call(b)) return false;
+
+            // If items are not an object or array, return false
+            if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
+
+            // Compare the length of the length of the two items
+            var aLen = type === '[object Array]' ? a.length : Object.keys(a).length;
+            var bLen = type === '[object Array]' ? b.length : Object.keys(b).length;
+            if (aLen !== bLen) return false;
+
+            // Compare two items
+            var compare = function (item1, item2) {
+                // Get the object type
+                var itemType = Object.prototype.toString.call(item1);
+
+                // If an object or array, compare recursively
+                if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+                    if (!Util.object.isEquivalent(item1, item2)) return false;
+                }
+
+                // Otherwise, do a simple comparison
+                else {
+
+                    // If the two items are not the same type, return false
+                    if (itemType !== Object.prototype.toString.call(item2)) return false;
+
+                    // Else if it's a function, convert to a string and compare
+                    // Otherwise, just compare
+                    if (itemType === '[object Function]') {
+                        if (item1.toString() !== item2.toString()) return false;
+                    } else {
+                        if (item1 !== item2) return false;
+                    }
+
+                }
+            };
+
+            // Compare properties
+            if (type === '[object Array]') {
+                for (let i = 0; i < aLen; i++) {
+                    if (compare(a[i], b[i]) === false) return false;
+                }
+            } else {
+                for (let key in a) {
+                    if (a.hasOwnProperty(key)) {
+                        if (compare(a[key], b[key]) === false) return false;
+                    }
+                }
+            }
+
+            // If nothing failed, return true
+            return true;
+        }
+    },
+    // Handles arrays
+    array: {
+        // Checks if all values are the same
+        allValuesSame: function (a, b = undefined) {
+            for (let i = 0, len = a.length; i < len; i++)
+                if (a[i] !== (typeof b !== 'undefined' ? b : a[0])) return false;
+            return true;
+        }
     }
 };
